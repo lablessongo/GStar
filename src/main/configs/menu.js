2479c6c316d8be3f7c3ba5ff5d305468715e18e9
surferrer <30338529+surferrer@users.noreply.github.com>
2019-07-10 09:57:33 +0800

    菜单按钮增加

---
+++ src/main/configs/menu.js
@@ -0,0 +1,57 @@
+import { app, Menu, BrowserWindow } from "electron";
+
+const menuTemplate = [
+    {
+        label: "Application",
+        submenu: [
+            {
+                label: "About GStar",
+                selector: "orderFrontStandardAboutPanel:"
+            },
+            { type: "separator" },
+            {
+                label: "Quit",
+                accelerator: "Command+Q",
+                click: function() {
+                    app.quit();
+                }
+            }
+        ]
+    },
+    {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            {
+                label: "Redo",
+                accelerator: "Shift+CmdOrCtrl+Z",
+                selector: "redo:"
+            },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            {
+                label: "Select All",
+                accelerator: "CmdOrCtrl+A",
+                selector: "selectAll:"
+            }
+        ]
+    },
+    {
+        label: "View",
+        submenu: [
+            {
+                label: "Open DevTools",
+                accelerator: "Command+D",
+                click: function() {
+                    BrowserWindow.getFocusedWindow().webContents.openDevTools();
+                }
+            }
+        ]
+    }
+];
+
+export const setAppMenu = () => {
+    Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
+};
